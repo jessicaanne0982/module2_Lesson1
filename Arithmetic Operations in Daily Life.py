9d0e1385a35fbb1147700d4a2eb087976616e073
Jessica Ulrich <jessicaanne0982@gmail.com>
2024-10-06 05:55:43 -0400

    This is my Module 2, Lesson 1 Assignment.

---
+++ Arithmetic Operations in Daily Life.py
@@ -0,0 +1,17 @@
+# Task 1:
+
+bread_price = 2.50
+peanut_butter_price = 4
+jelly_price = 3.25
+
+total_cost = bread_price + peanut_butter_price + jelly_price
+print(total_cost)
+
+# Task 2: 
+
+initial_balance = 10000
+interest_rate = 7 / 100
+interest_accrued = initial_balance * interest_rate
+
+ending_balance = initial_balance + interest_accrued
+print(ending_balance)
---
+++ Showcase Your Dance Moves.py
@@ -0,0 +1,18 @@
+# Task 1 : 
+
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else: print("Take an umbrella!")
+
+# Task 2: 
+
+mood = input("How are you feeling today?")
+
+if mood == "happy":
+    print("That's great to hear!")
+elif mood == "sad":
+    print("I hope your day gets better!")
+
+    
